f8c2001b0151d1fbc4eab0b7dce7f81da9bcc214
Alan Nouman <tsoloevalan@gmail.com>
2025-06-19 18:26:49 +0300

    3

--- drfsite/goods/views.py
+++ drfsite/goods/views.py
@@ -1,10 +1,23 @@
-
+from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.views import APIView
+from django.forms.models import model_to_dict
+
 # Create your views here.
 
 from .models import Goods
 from .serializers import GoodsSerializer
 
-class GoodsAPIView(generics.ListAPIView):
-    queryset = Goods.objects.all()
-    serializer_class = GoodsSerializer
+class GoodsAPIView(APIView):
+    def get(self, request):
+        lst = Goods.objects.all().values()
+        return Response({"goods": list(lst)})
+    def post(self, request):
+        post_new = Goods.objects.create(name=request.data['name'],
+                                         cats_id=request.data['cats_id'])
+        return Response({"post":model_to_dict(post_new)})
+
+
+# class GoodsAPIView(generics.ListAPIView):
+#     queryset = Goods.objects.all()
+#     serializer_class = GoodsSerializer
